0361f9db242bdde31df0dcc4bab819f351b26a3a
J Austin Tobin <neophoebus@gmail.com>
2015-02-01 20:27:14 -0600

    Validate BST problem and test added

---
+++ src/com/algspractice/node/probs/ValidateBinaryTree.java
@@ -0,0 +1,24 @@
+package com.algspractice.node.probs;
+
+import com.algspractice.node.ds.TreeNode;
+
+/**
+ * Created by jtobin on 01/02/15.
+ */
+public class ValidateBinaryTree {
+
+    public static boolean isValidBST(TreeNode root) {
+        return validate(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public static boolean validate(TreeNode root, Integer min, Integer max) {
+        //null is a valid binary tree, and if there is no node, then that is the base
+        //case and ist still valid
+        if (root == null) return true;
+        //check the validity of the binary tree.
+        //the nodes value cannot be greater than the minimum, and the node cannot be greater than the maximum
+        if ((Integer) root.getVal() <= min || (Integer) root.getVal() >= max) return false;
+        return validate(root.getLeft(), min, (Integer) root.getVal()) && validate(root.getRight(), (Integer) root.getVal(), max );
+
+    }
+}
---
+++ src/com/algspractice/node/ut/ValidateBinaryTreeTest.java
@@ -0,0 +1,66 @@
+package com.algspractice.node.ut;
+
+import com.algspractice.node.ds.TreeNode;
+import com.algspractice.node.probs.ValidateBinaryTree;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.swing.tree.TreeModel;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by jtobin on 01/02/15.
+ */
+public class ValidateBinaryTreeTest {
+
+    private TreeNode<Integer> negRoot;
+    private TreeNode<Integer> posRoot;
+    private TreeNode<Integer> nullRoot;
+    @Before
+    public void before() {
+
+        //create positive case
+        TreeNode<Integer> root = new TreeNode<Integer>(8);
+        root.setLeft(new TreeNode<Integer>(3));
+        root.setRight(new TreeNode<Integer>(10));
+        posRoot = root;
+        root = root.getLeft();
+        root.setLeft(new TreeNode<Integer>(1));
+        root.setRight(new TreeNode<Integer>(6));
+        root = root.getRight();
+        root.setLeft(new TreeNode<Integer>(4));
+        root.setRight(new TreeNode<Integer>(7));
+        root = posRoot.getRight();
+        root.setRight(new TreeNode<Integer>(14));
+        root = root.getRight();
+        root.setLeft(new TreeNode<Integer>(13));
+
+
+
+
+        //create negative case
+        root = new TreeNode<Integer>(20);
+        root.setLeft(new TreeNode<Integer>(10));
+        root.setRight(new TreeNode<Integer>(30));
+        negRoot = root;
+        root = root.getRight();
+        root.setLeft(new TreeNode<Integer>(5));
+        root.setRight(new TreeNode<Integer>(40));
+
+        //create null base case
+        nullRoot = null;
+    }
+
+    @Test
+    public void testPositiveCase() {
+        assertTrue(ValidateBinaryTree.isValidBST(posRoot));
+    }
+
+    @Test
+    public void testNegativeCase() {
+        assertFalse(ValidateBinaryTree.isValidBST(negRoot));
+    }
+}
